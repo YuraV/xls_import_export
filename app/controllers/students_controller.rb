c094c5794adc2a0033d9d9bac4be6c72164e5f15
Yurii Vodolyak <yurii.vodolyak@onapp.com>
2014-04-07 20:04:06 +0300

    first success

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,15 @@
 class StudentsController < ApplicationController
+  def index
+    @students = Student.order(:initials)
+    respond_to do |format|
+      format.html
+      format.csv {send_data @students.to_csv}
+      format.xls
+    end
+  end
+
+  def import
+    Student.import(params[:file])
+    redirect_to root_url, notice: 'Students Imported.'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,11 @@ XlsImportExport::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  resources :students do
+    post :import, on: :collection
+  end
+
+  root :to => 'students#index'
 
   # See how all your routes lay out with "rake routes"
 
