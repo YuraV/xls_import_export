98e461728ff09660a1fce23c6152237154c68b1d
Yurii Vodolyak <yurii.vodolyak@onapp.com>
2014-04-22 22:58:10 +0300

    fix aspirant_left_table

--- app/models/aspirat_left.rb
+++ app/models/aspirat_left.rb
@@ -1,3 +1,3 @@
 class AspiratLeft < ActiveRecord::Base
-  attr_accessible :faculty, :initials, :nakaz_rektora, :shufr_i_nazva_specialnosti, :z_vidruvom_vid_vurobnyctva
+  attr_accessible :faculty, :initials, :nakaz_rektora, :shufr_i_nazva_specialnosti, :vidrahyvanja_vid_chusla, :z_vidruvom_vid_vurobnyctva
 end
